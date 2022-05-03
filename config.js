8fb1fedc4ea33aba1801cf7a64fccb4f3f82f4a0
MareoftheStars <diez.sean.pro@gmail.com>
2022-05-03 09:06:39 +0200

    UPDATED API KEY AND Various changes

--- config.js
+++ config.js
@@ -1,30 +1,30 @@
 module.exports = {
-    account_name: 'hellersdorf',
-    endpoint: 'netlify/passave',
-    queries: [
-        {
-            query: `{
-                getPosts {
-                    title
-                    body
-                    published
-                    id
-                    categories {
-                        name
-                    }
-                }
-            }`,
-            convert_to: '.md',
-            slug_field: 'title',
-            body_field: 'body',
-            folder: 'content/blog',
-        },
-    ],
+  account_name: 'hellersdorf',
+  endpoint: '/netlify/passave',
+  queries: [
+    {
+      query: `{
+            getPosts {
+              title
+              body
+              published
+              id
+              categories {
+                name
+              }
+            }
+          }`,
+      convert_to: '.md',
+      slug_field: 'title',
+      body_field: 'body',
+      folder: 'content/blog',
+    },
+  ],
 };
 
-                // getAccounts {
-                //     id
-                //     username
-                //     password
-                //     email
-                // }
+// getAccounts {
+//     id
+//     username
+//     password
+//     email
+// }
--- pullContent.js
+++ pullContent.js
@@ -2,35 +2,22 @@ const https = require('https');
 const fs = require('fs');
 const slugify = require('slugify');
 
-
 const data = JSON.stringify({
-    query: `{
-      getPosts {
-        title
-        body
-        published
-        id
-        categories {
-          name
-        }
+  query: `{
+    getPosts {
+      title
+      body
+      published
+      id
+      categories {
+        name
       }
-    }`,
-  });
-
-// const data = JSON.stringify({
-//     query: `{
-//         getAccounts {
-//             id
-//             username
-//             password
-//             email
-//         }
-//     }`,
-// });
+    }
+  }`,
+});
 
 const markdownFileTemplate = (post, categories) => `
 ---
-account: ${account.name}
 title: ${post.title}
 categories:  ${categories}
 published: ${post.published}
@@ -40,47 +27,43 @@ ${post.body}
 `;
 
 const options = {
-    hostname: 'hellersdorf.stepzen.net',
-    path: '/netlify/passave/__graphql',
-
-    // hostname: 'eu-cdbr-west-02.cleardb.net',
-    // user: 'b0fd8124c08313',
-    // password: '25d41bba',
-    // database: 'heroku_4c75c660385627e',
-    
-    port: 443,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length,
-        Authorization: 'Apikey ' + process.env.STEPZEN_API_KEY,
-        'User-Agent': 'Node',
-    },
+  hostname: 'hellersdorf.stepzen.net',
+  path: '/netlify/passave/__graphql',
+  port: 443,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length,
+    // Authorization: 'Apikey ' + process.env.STEPZEN_API_KEY,
+    Authorization: 'Apikey hellersdorf::stepzen.net+1000::1f0d97bb8ce3431c80ba7c33ee8dd8887b76203f962bd7de7c3f9cfbf8806da0',
+    'User-Agent': 'Node',
+  },
 };
 
 const req = https.request(options, (res) => {
-    let data = '';
+  let data = '';
 
-    res.on('data', (d) => {
-        data += d;
+  console.log(" ===API KEY : " + process.env.STEPZEN_API_KEY)
+
+  res.on('data', (d) => {
+    data += d;
+  });
+  res.on('end', () => {
+    const results = JSON.parse(data).data.getPosts;
+    results.forEach((post) => {
+      let categories = '';
+      post.categories.forEach((category) => {
+        categories += '\n- ' + category.name;
+      });
+      let content = markdownFileTemplate(post, categories);
+      let filename = './content/blog/' + slugify(post.title) + '.md';
+      fs.writeFileSync(filename, content);
     });
-    console.log(`   ===DATA: ${data}\n`);
-    res.on('end', () => {
-        const results = JSON.parse(data).data.getPosts;
-        results.forEach((post) => {
-        let categories = '';
-        post.categories.forEach((category) => {
-            categories += '\n- ' + category.name;
-        });
-        let content = markdownFileTemplate(post, categories);
-        let filename = './content/blog/' + slugify(post.title) + '.md';
-        fs.writeFileSync(filename, content);
-        });
-    });
+  });
 });
 
 req.on('error', (error) => {
-    console.error(error);
+  console.error(error);
 });
 
 req.write(data);
